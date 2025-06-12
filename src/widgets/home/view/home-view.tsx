862f5d46f0e2b7b58eb15db1b95285f985e17d44
huynhkien <huynhhuukien2962@gmail.com>
2025-06-12 17:00:47 +0700

    Build widget view home for website

---
+++ src/widgets/home/view/home-view.tsx
@@ -0,0 +1,12 @@
+import { HomeAbout, HomePortfolio, HomeProduct, HomeSlides } from '../../index';
+
+export const HomeView = () => {
+    return (
+        <>
+            <HomeSlides/>
+            <HomePortfolio/>
+            <HomeAbout/>
+            <HomeProduct/>
+        </>
+    )
+}

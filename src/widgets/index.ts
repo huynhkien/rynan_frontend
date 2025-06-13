5811b97df9ef6e96ae6bc4a3a2bb965f952d2899
huynhkien <huynhhuukien2962@gmail.com>
2025-06-13 14:32:43 +0700

    file index.ts

---
+++ src/widgets/index.ts
@@ -0,0 +1,13 @@
+// Home
+export * from './home/view/home-view';
+export * from './home/ui/home-slides';
+export * from './home/ui/home-portfolio';
+export * from './home/ui/home-about';
+export * from './home/ui/home-product';
+export * from './home/ui/home-banner';
+export * from './home/ui/home-whyUs';
+export * from './home/ui/home-testimonial';
+export * from './home/ui/home-partner';
+
+// product
+export * from './product/ui/product-card';

e1fa467c0e99d5c1e9248d091d7ab372721d80e7
huynhkien <huynhhuukien2962@gmail.com>
2025-06-16 17:00:59 +0700

    Build interface about for website

---
+++ src/app/(main)/about/page.tsx
@@ -0,0 +1,15 @@
+import { Banner } from "@/shared/components/layout/Banner";
+import {Container } from "@mui/material";
+
+const About = () => {
+  return (
+    <>
+        <Banner/>
+        <Container>
+            Chào mừng bạn đến với Rynan Smart Agriculture
+        </Container>
+    </>
+  );
+}
+
+export default About;

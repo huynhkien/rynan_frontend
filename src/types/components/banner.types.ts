547b8109bc57146174fccf91d895315df8d8ad4a
huynhkien <huynhhuukien2962@gmail.com>
2025-06-17 17:02:09 +0700

    Add types component

---
+++ src/types/components/banner.types.ts
@@ -0,0 +1,7 @@
+export interface BannerProps{
+    category?: string,
+    breadcrumb:[{
+        title: string,
+        url: string
+    }]
+}
---
+++ src/types/components/inputForm.types.ts
@@ -0,0 +1,15 @@
+export interface InputFormProps {
+  id?: string;
+  sx?: React.CSSProperties;
+  value: string;
+  setValue: (value: string) => void;
+  nameKey: string;
+  type?: string;
+  multiline?: boolean;
+  rows?: number;
+  invalidFields?: Array<{ name: string; message: string }>;
+  setInValidFields?: React.Dispatch<React.SetStateAction<Array<{ name: string; message: string }>>>;
+  iconClass?: React.ReactNode;
+  placeholder?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}

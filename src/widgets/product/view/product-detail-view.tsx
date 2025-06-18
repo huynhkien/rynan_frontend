cf850975e5c4ead71d3e6fe810c7d25fda5ea6f8
huynhkien <huynhhuukien2962@gmail.com>
2025-06-18 16:11:38 +0700

    Update product detail view view

---
+++ src/widgets/product/view/product-detail-view.tsx
@@ -0,0 +1,22 @@
+import { Banner } from "@/shared/components/layout/Banner"
+import { ProductDetailInfo } from "../ui/product-detail-info"
+import { Container } from "@mui/material"
+import { ProductDetailTab } from "../ui/product-detail-tab"
+
+export const ProductDetailView = () => {
+    return (
+        <>
+        <Banner
+                category='Chi tiết sản phẩm'
+                breadcrumb={[
+                    { title: 'Sản phẩm', url: '/products' },
+                    { title: 'Chi tiết sản phẩm', url: '/products/123' }
+                ]}
+            />
+            <Container maxWidth='xl' sx={{py:5}}>
+                <ProductDetailInfo/>
+                <ProductDetailTab/>
+            </Container>
+        </>
+    )
+}

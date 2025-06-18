9d5851b5e68579cde84346c675d7c877d117f9ec
huynhkien <huynhhuukien2962@gmail.com>
2025-06-18 16:05:45 +0700

    Add files

---
+++ src/shared/constant/product-sort.ts
@@ -0,0 +1,32 @@
+export const ProductSortOption = [
+    {
+        id: 1,
+        value: "name",
+        text: "Theo bảng chữ cái, A-Z"
+    },
+    {
+        id: 2,
+        value: "-name",
+        text: "Theo bảng chữ cái, Z-A"
+    },
+    {
+        id: 3,
+        value: "-price",
+        text: "Giá từ cao đến thấp"
+    },
+    {
+        id: 4,
+        value: "price",
+        text: "Giá từ thấp đến cao"
+    },
+    {
+        id: 5,
+        value: "-createdAt",
+        text: "Ngày, mới đến cũ"
+    },
+    {
+        id: 6,
+        value: "-sold",
+        text: "Theo lượt mua"
+    },
+]
---
+++ src/shared/constant/vote-star.ts
@@ -0,0 +1,22 @@
+export const VoteStar = [
+    {
+        id: 1,
+        text: 'Tệ'
+    },
+    {
+        id: 2,
+        text: 'Trung bình'
+    },
+    {
+        id: 3,
+        text: 'Khá'
+    },
+    {
+        id: 4,
+        text: 'Tốt'
+    },
+    {
+        id: 5,
+        text: 'Xuất sắc'
+    },
+]

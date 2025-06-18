1d0b6e538ced4541bfeaf7856a2e3999ab38365c
huynhkien <huynhhuukien2962@gmail.com>
2025-06-18 16:07:46 +0700

    Update widget view for page login

--- src/widgets/login/view/login-view.tsx
+++ src/widgets/login/view/login-view.tsx
@@ -1,24 +1,40 @@
 import { Box } from "@mui/material"
+import { LoginForm } from "../ui/login-form"
+import { Breadcrumb } from "@/shared/components"
 
 export const LoginView = () => {
     return (
-        <Box
-            sx={{
-                width: '100%',
-                height: '700px',
-                position: 'relative',
-                '&::after': {
-                    content:'""',
-                    backgroundImage: 'url(/banner/banner-4.jpg)',
-                    position: 'absolute',
-                    height: '100%',
+        <>
+            <Box
+                sx={{
                     width: '100%',
-                    top: 0,
-                    left: 0
-                }
-            }}
-        >
-            Trang đăng nhập
-        </Box>
+                    height: '800px',
+                    position: 'relative',
+                    pb: 15,
+                    '&::after': {
+                        content:'""',
+                        backgroundImage: 'url(/banner/banner-4.jpg)',
+                        position: 'absolute',
+                        height: '100%',
+                        width: '100%',
+                        top: 0,
+                        left: 0,
+                        zIndex: -1
+                    }
+                }}
+            >
+                <Box
+                    sx={{ml:25}}
+                >
+                    <Breadcrumb
+                        breadcrumb={[{
+                            name: 'Đăng Nhập',
+                            url: '/login'
+                        }]}
+                    />
+                </Box>
+                <LoginForm/>
+            </Box>
+        </>
     )
 }

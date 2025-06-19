33412fd18475f0dc97c088cec20f09a2141e3285
huynhkien <huynhhuukien2962@gmail.com>
2025-06-19 15:14:36 +0700

    Update layouts main

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,17 +1,14 @@
 import { Footer, Header } from '@/shared/components'
-import { Box } from '@mui/material'
 import React from 'react'
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div>
-        <Header/>
-        <Box>
-          {children}
-        </Box>
-        <Footer/>
-    </div>
+      <Header />
+      <main>
+        {children}
+      </main>
+      <Footer />
     </>
   )
 }

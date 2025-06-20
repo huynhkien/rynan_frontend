cdc08362490c5d8dc8c6a6d7de563f455f7ce939
huynhkien <huynhhuukien2962@gmail.com>
2025-06-20 17:01:08 +0700

    Build widget view category-management for admin page

---
+++ src/widgets/private/category-management/view/category-management-view.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { Breadcrumb } from "@/shared/components"
+import { Box, useTheme } from "@mui/material"
+import { CategoryManagementFormList } from "../ui/category-management-form-list";
+
+export const CategoryManagementView = () => {
+    const theme = useTheme();
+    return (
+        <>
+            <Box
+                sx={{
+                    backgroundColor: theme.palette.background.default,
+                    width: '100%'
+                }}
+            >
+                <Breadcrumb
+                    type='Admin'
+                    breadcrumb={[{
+                        name: 'Quản lý danh mục',
+                        url: '/admin/category-management',
+                    }]}
+                />
+            </Box>
+            <Box>
+                <CategoryManagementFormList/>
+            </Box>
+        </>
+    )
+}

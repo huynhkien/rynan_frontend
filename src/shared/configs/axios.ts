0b2363f92c07f6f2445583af2e7766b382e75721
huynhkien <huynhhuukien2962@gmail.com>
2025-06-23 17:07:21 +0700

    Build file connect with api backend

---
+++ src/shared/configs/axios.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { Env } from "./env";
+
+const axiosInstance = axios.create({
+  baseURL: Env.API_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  }
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const localStorageData = window.localStorage.getItem("persist:shop/user");
+
+    if (localStorageData) {
+      try {
+        const parsedData = JSON.parse(localStorageData);
+        const accessToken = JSON.parse(parsedData?.token || 'null');
+
+        if (accessToken) {
+          config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+      } catch (error) {
+        console.error("Lỗi:", error);
+      }
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response?.data,
+  (error) => {
+    return Promise.reject(error?.response?.data || error);
+  }
+);
+
+export default axiosInstance;

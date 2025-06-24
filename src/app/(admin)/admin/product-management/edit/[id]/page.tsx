b00c510fb790cb43c6b5af952d6f965fbf833236
huynhkien <huynhhuukien2962@gmail.com>
2025-06-25 06:49:53 +0700

    Update structure page admin

---
+++ src/app/(admin)/admin/product-management/edit/[id]/page.tsx
@@ -0,0 +1,10 @@
+import { ProductManagementAddView } from "@/widgets/private/product-management/view/product-management-add-view"
+
+const ProductManagementEdit = () => {
+    return (
+        <>
+            <ProductManagementAddView/>
+        </>
+    )
+}
+export default ProductManagementEdit;

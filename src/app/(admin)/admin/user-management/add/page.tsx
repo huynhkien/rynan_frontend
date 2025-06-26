36c168da003bb4c37066dd8063a73c295450bfe5
huynhkien <huynhhuukien2962@gmail.com>
2025-06-27 06:20:08 +0700

    Update structure admin pages

---
+++ src/app/(admin)/admin/user-management/add/page.tsx
@@ -0,0 +1,10 @@
+import { UserManagementAddView } from "@/widgets/private/user-management/view/user-management-add-view";
+
+const UserManagementAdd = () => {
+    return (
+        <>
+            <UserManagementAddView/>
+        </>
+    )
+}
+export default UserManagementAdd;

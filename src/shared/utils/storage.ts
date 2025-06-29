87f4427ac509a2988166a70d556477737c4ed013
huynhkien <huynhhuukien2962@gmail.com>
2025-06-29 20:56:43 +0700

    Update structure utils

--- src/shared/utils/storage.ts
+++ src/shared/utils/storage.ts
@@ -1,7 +1,6 @@
 
 import createWebStorage from "redux-persist/es/storage/createWebStorage";
 
-
 // khi chạy trên server, nó giả lập localStorage nhưng không lưu gì cả => tránh lỗi mà vẫn cho redux hoạt động
 const createNoopStorage = () => {
     return {

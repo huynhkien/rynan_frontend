a06f605c1a6933758813f66680514ce450a7f7de
Kien <155994022+huynhkien@users.noreply.github.com>
2025-06-30 17:02:19 +0700

    Update env.ts

--- src/shared/configs/env.ts
+++ src/shared/configs/env.ts
@@ -1,4 +1,4 @@
 export const Env = {
-    API_URL: "https://rynan-backend.onrender.com/api",
+    API_URL: "https://rynan-backend.onrender.com/api/",
     NEXT_URL: process.env.NEXT_PUBLIC_NEXT_URL || "http://localhost:3000"
 }

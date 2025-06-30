1b5c77753668dc4c1e1f192b44f53ff3ec2eb193
huynhkien <huynhhuukien2962@gmail.com>
2025-06-30 16:52:58 +0700

    Update structure admin page

---
+++ src/app/(admin)/admin/quote-management/edit/[id]/page.tsx
@@ -0,0 +1,10 @@
+import { QuoteManagementEditView } from "@/widgets/private/quote-management/view/quote-management-edit-view"
+
+const QuoteManagementEdit = () => {
+    return (
+        <>
+            <QuoteManagementEditView/>
+        </>
+    )
+}
+export default QuoteManagementEdit;

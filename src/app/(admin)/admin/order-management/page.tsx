1c926c6f791f14f82fa06d23dbaf1259bd8de721
huynhkien <huynhhuukien2962@gmail.com>
2025-07-01 17:01:20 +0700

    Build interface order management page

---
+++ src/app/(admin)/admin/order-management/page.tsx
@@ -0,0 +1,10 @@
+import { OrderManagementView } from "@/widgets/private/order-management/view/order-management-view"
+
+const OrderManagement = () => {
+    return (
+        <>
+            <OrderManagementView/>
+        </>
+    )
+}
+export default OrderManagement;

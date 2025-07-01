5e517ad3f2cd0437c5b989e3a3cbedc2a64fb65f
huynhkien <huynhhuukien2962@gmail.com>
2025-07-01 22:14:28 +0700

    Update structure admin

---
+++ src/app/(admin)/admin/order-management/add/page.tsx
@@ -0,0 +1,10 @@
+import { OrderManagementAddView } from "@/widgets/private/order-management/view/order-management-add-view";
+
+const OrderManagementAdd = () => {
+    return (
+        <>
+            <OrderManagementAddView/>
+        </>
+    )
+}
+export default OrderManagementAdd;

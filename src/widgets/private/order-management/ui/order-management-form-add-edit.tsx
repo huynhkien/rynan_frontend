5909cf0af536b06bd16cd591e610a7c07218da0e
huynhkien <huynhhuukien2962@gmail.com>
2025-07-01 17:04:12 +0700

    Add ui order management

---
+++ src/widgets/private/order-management/ui/order-management-form-add-edit.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material"
+
+export const OrderManagementFormAddEdit = () => {
+    return (
+        <Box>
+            Htển thị thông tin form thêm và chỉnh sửa đơn hàng
+        </Box>
+    )
+}
---
+++ src/widgets/private/order-management/ui/order-management-form-list.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material"
+
+export const OrderManagementFormList = () => {
+    return (
+        <Box>
+            Hiển thị thông tin đơn hàng
+        </Box>
+    )
+}

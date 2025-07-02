91403000299a149f5ab46b15b81667ae536d10bb
huynhkien <huynhhuukien2962@gmail.com>
2025-07-02 17:01:15 +0700

    Update structure admin pages

---
+++ src/app/(admin)/admin/order-management/edit/[id]/page.tsx
@@ -0,0 +1,10 @@
+import { OrderManagementEditView } from "@/widgets/private/order-management/view/order-management-edit-view";
+
+const OrderManagementEdit = () => {
+    return (
+        <>
+            <OrderManagementEditView/>
+        </>
+    )
+}
+export default OrderManagementEdit;

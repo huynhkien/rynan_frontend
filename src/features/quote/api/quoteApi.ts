0b4a194d1da630551fd6e30b6f2faf23f2999501
huynhkien <huynhhuukien2962@gmail.com>
2025-07-02 17:03:03 +0700

    Update structure product quote

--- src/features/quote/api/quoteApi.ts
+++ src/features/quote/api/quoteApi.ts
@@ -2,23 +2,26 @@ import axiosInstance from "@/shared/configs/axios";
 import { QuoteEndpoints } from "./quoteEndpoints";
 import { QuoteData, QuoteResponse, QuotesResponse } from "../type/quoteType";
 
-// Thêm danh mục
+// Thêm báo giá
 export const createQuote = async (data: QuoteData): Promise<QuoteResponse> => 
   axiosInstance.post(QuoteEndpoints.CREATE, data, {
     withCredentials: true,
     
 });
-// Cập nhật danh mục
+// Cập nhật báo giá
 export const updateQuote = async (data: QuoteData, id: string): Promise<QuoteResponse> => 
   axiosInstance.put(QuoteEndpoints.UPDATE(id), data, {
     withCredentials: true,
 });
-// Hiển thị tất cả danh mục
+// Hiển thị tất cả báo giá
 export const getAllQuote = async(): Promise<QuotesResponse> => 
     axiosInstance.get(QuoteEndpoints.GET_ALL);
-// Lấy danh mục theo id
+// Lấy báo giá theo id
 export const getQuoteById = async(id: string): Promise<QuoteResponse> => 
     axiosInstance.get(QuoteEndpoints.GET_BY_ID(id));
-// Xóa danh mục
+// Xóa báo giá
 export const deleteQuote = async(id: string): Promise<QuoteResponse> => 
     axiosInstance.delete(QuoteEndpoints.DELETE(id))
+// Xóa sản phẩm báo giá
+export const deleteProductQuote = async(id: string, pid: string): Promise<QuoteResponse> => 
+    axiosInstance.delete(QuoteEndpoints.DELETE_PRODUCT_QUOTE(id, pid))
--- src/features/quote/api/quoteEndpoints.ts
+++ src/features/quote/api/quoteEndpoints.ts
@@ -4,4 +4,5 @@ export const QuoteEndpoints = {
     GET_BY_ID: (id: string | number) => `/quote/${id}`,
     UPDATE: (id: string | number) => `/quote/${id}`,
     DELETE: (id: string | number) => `/quote/${id}`,
+    DELETE_PRODUCT_QUOTE: (id: string | number, pid: string | number) => `/quote/delete-product-quote/${id}/${pid}`,
 }

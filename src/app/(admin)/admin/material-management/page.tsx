d852d26c1205b4580160539cbd1573be5b9fc502
huynhkien <huynhhuukien2962@gmail.com>
2025-07-03 17:04:17 +0700

    Build interface material management

---
+++ src/app/(admin)/admin/material-management/page.tsx
@@ -0,0 +1,10 @@
+import { MaterialManagementView } from "@/widgets/private/material-management/view/material-management-view"
+
+const MaterialManagement = () => {
+    return (
+        <>
+            <MaterialManagementView/>
+        </>
+    )
+}
+export default MaterialManagement;

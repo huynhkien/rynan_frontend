ceee5f7698ad08947fae07f6a3c8627f857d7712
huynhkien <huynhhuukien2962@gmail.com>
2025-07-03 23:06:56 +0700

    Update structure

---
+++ src/app/(admin)/admin/supplier-management/page.tsx
@@ -0,0 +1,10 @@
+import { SupplierManagementView } from "@/widgets/private/supplier-management/view/supplier-management-view";
+
+const MaterialManagement = () => {
+    return (
+        <>
+            <SupplierManagementView/>
+        </>
+    )
+}
+export default MaterialManagement;

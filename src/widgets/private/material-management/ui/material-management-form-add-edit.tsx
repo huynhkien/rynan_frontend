a4432b1fdd266556eefb7415105693e46638e550
huynhkien <huynhhuukien2962@gmail.com>
2025-07-03 17:06:32 +0700

    Build widgets material-management

---
+++ src/widgets/private/material-management/ui/material-management-form-add-edit.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material"
+
+export const MaterialManagementFormAddEdit = () => {
+    return (
+        <Box>
+            Add and Edit
+        </Box>
+    )
+}
---
+++ src/widgets/private/material-management/view/material-management-view.tsx
@@ -0,0 +1,28 @@
+'use client'
+import { Box, useTheme } from "@mui/material"
+import { MaterialManagementFormList } from "../ui/material-management-form-list"
+import { Breadcrumb } from "@/shared/components"
+
+export const MaterialManagementView = () => {
+    const theme = useTheme();
+    return (
+        <>
+            <Box
+                sx={{
+                    backgroundColor: theme.palette.background.default,
+                    width: '100%'
+                }}
+            >
+                <Breadcrumb
+                    type='Admin'
+                    breadcrumb={[{
+                        name: 'Quản lý danh sách nguyên liệu',
+                        url: '/admin/receipt-management',
+                    },
+                ]}
+                />
+            </Box>
+            <MaterialManagementFormList/>
+        </>
+    )
+}

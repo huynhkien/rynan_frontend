f43a93b7931adc039ec989fde0873e23d398fc82
huynhkien <huynhhuukien2962@gmail.com>
2025-07-08 17:06:54 +0700

    Build interface inventory management

---
+++ src/app/(admin)/admin/inventory-management/page.tsx
@@ -0,0 +1,10 @@
+import { InventoryManagementView } from "@/widgets/private/inventory-management/view/inventory-management-view";
+
+const InventoryManagement = () => {
+    return (
+        <>
+            <InventoryManagementView/>
+        </>
+    )
+}
+export default InventoryManagement;

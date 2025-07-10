515bbd187474ba92baa9603bb101970212349dfa
huynhkien <huynhhuukien2962@gmail.com>
2025-07-10 11:02:18 +0700

    Update structure admin pages

---
+++ src/app/(admin)/admin/quote-management/potential_customer/page.tsx
@@ -0,0 +1,10 @@
+import { QuoteManagementPotentialCustomerView } from "@/widgets/private/quote-management/view/quote-management-potential-customer-view"
+
+const QuoteManagementPotentialCustomer = () => {
+    return (
+        <>
+            <QuoteManagementPotentialCustomerView/>
+        </>
+    )
+}
+export default QuoteManagementPotentialCustomer;

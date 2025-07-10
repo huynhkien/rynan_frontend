4225f26ad297df7a415769d4a3e6ebebfe4db5cd
huynhkien <huynhhuukien2962@gmail.com>
2025-07-10 11:06:11 +0700

    Update structure config font

--- src/shared/configs/font.ts
+++ src/shared/configs/font.ts
@@ -2,7 +2,7 @@ import localFont from 'next/font/local';
 
 // Cấu hình font chữ cho website
 const utmAvo = localFont({
-  src: '../../../public/font/UTM Avo.ttf', 
+  src: '../../../public/fonts/UTM Avo.ttf', 
   variable: '--font-utm-avo',
   display: 'swap',
 });

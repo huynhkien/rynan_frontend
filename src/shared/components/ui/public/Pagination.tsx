1f1a772f58d8e5f50d26ebfef0fac1a9742adbb8
huynhkien <huynhhuukien2962@gmail.com>
2025-07-10 21:24:46 +0700

    Update structure components

--- src/shared/components/ui/public/Pagination.tsx
+++
@@ -1,9 +0,0 @@
-import { Box, Pagination as PaginationItem } from "@mui/material"
-
-export const Pagination = () => {
-    return (
-        <Box>
-            <PaginationItem count={10} variant="outlined" />
-        </Box>
-    )
-}

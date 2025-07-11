ec7d5e0fa4dba92e568c952c042a0c853c330666
huynhkien <huynhhuukien2962@gmail.com>
2025-07-11 14:41:18 +0700

    Update structure admin page

---
+++ src/app/(main)/search/[value]/page.tsx
@@ -0,0 +1,10 @@
+import { ProductSearchView } from "@/widgets/public/product/view/products-search-view";
+
+const Search = () => {
+    return (
+        <>
+            <ProductSearchView/>
+        </>
+    )
+}
+export default Search;

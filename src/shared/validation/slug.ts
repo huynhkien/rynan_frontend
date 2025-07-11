80b92a599b54e5534529270987c41067230c6701
huynhkien <huynhhuukien2962@gmail.com>
2025-07-11 14:43:56 +0700

    Add validation file slug.ts

---
+++ src/shared/validation/slug.ts
@@ -0,0 +1,10 @@
+export const slugify = (str: string) => {
+    return str
+          .toLowerCase()
+          .normalize("NFD")                
+          .replace(/[\u0300-\u036f]/g, '') 
+          .replace(/\s+/g, '-')            
+          .replace(/[^a-z0-9-]/g, '')      
+          .replace(/--+/g, '-')            
+          .replace(/^-+|-+$/g, '');        
+    };

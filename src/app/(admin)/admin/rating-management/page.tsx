38b35bee44ad6543b722e39beced2ac2b1ac74f2
huynhkien <huynhhuukien2962@gmail.com>
2025-07-15 17:07:39 +0700

    Build interface rating management

---
+++ src/app/(admin)/admin/rating-management/page.tsx
@@ -0,0 +1,10 @@
+import { RatingManagementView } from "@/widgets/private/rating-management/view/rating-management-view"
+
+const RatingManagement = () => {
+    return (
+        <>
+            <RatingManagementView/>
+        </>
+    )
+}
+export default RatingManagement;

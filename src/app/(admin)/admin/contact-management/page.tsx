4402c50070d964d8a4a62393f6386b187b5a9f60
huynhkien <huynhhuukien2962@gmail.com>
2025-07-15 17:07:25 +0700

    Build interface contact management

---
+++ src/app/(admin)/admin/contact-management/page.tsx
@@ -0,0 +1,10 @@
+import { ContactManagementView } from "@/widgets/private/contact-management/view/contact-management-view"
+
+const ContactManagement = () => {
+    return(
+        <>
+            <ContactManagementView/>
+        </>
+    )
+}
+export default ContactManagement;

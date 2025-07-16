90786a7cff496c064c65c807445506b53f01bd8f
huynhkien <huynhhuukien2962@gmail.com>
2025-07-16 17:11:02 +0700

    Update structure auth

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,10 @@
+import { ForgotPasswordView } from "@/widgets/public/forgot-password/view/forgot-password-view"
+
+const ForgotPassword = () => {
+    return (
+        <>
+            <ForgotPasswordView/>
+        </>
+    )
+}
+export default ForgotPassword;
---
+++ src/app/(auth)/reset-password/[token]/page.tsx
@@ -0,0 +1,10 @@
+import { ResetPasswordView } from "@/widgets/public/reset-password/view/reset-password-view"
+
+const resetPassword = () => {
+    return (
+        <>
+            <ResetPasswordView/>
+        </>
+    )
+}
+export default resetPassword;

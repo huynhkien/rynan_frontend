2a0ea831d356597ecdaa774a6023e2cab01de15c
huynhkien <huynhhuukien2962@gmail.com>
2025-07-17 08:44:14 +0700

    update structure file middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,9 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get('accessToken')?.value
+  console.log(token);
   if (!token) {
-    return NextResponse.redirect(new URL('/login', request.url))
+    return NextResponse.redirect(new URL('/login', request.url));
   }
 
   try {

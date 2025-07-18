29bcdcb2097676d1b53e0580f03c3dd34c030848
huynhkien <huynhhuukien2962@gmail.com>
2025-07-18 16:57:58 +0700

    Update structure file axios.ts

--- src/shared/configs/axios.ts
+++ src/shared/configs/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: '/api',
-  timeout: 30000,
+  timeout: 50000,
   withCredentials: true,
   headers: {
     'Cache-Control': 'no-cache, no-store, must-revalidate',

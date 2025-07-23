8022e5002b0b4f45808e676b4e71dbfbdb027f43
huynhkien <huynhhuukien2962@gmail.com>
2025-07-23 16:51:20 +0700

    Update structure page main

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,12 +1,73 @@
+'use client'
 import { Footer, Header } from '@/shared/components'
-import React from 'react'
-
+import { ChatbotModelView } from '@/widgets/public/chatbot/view/chatbot-view'
+import { Cancel } from '@mui/icons-material'
+import { Dialog, Slide, Typography, useTheme } from '@mui/material'
+import { TransitionProps } from '@mui/material/transitions'
+import Image from 'next/image'
+import React, { forwardRef, Ref, useState } from 'react'
+const Transition = forwardRef<
+  Ref<unknown>,
+  TransitionProps & {
+    children: React.ReactElement;
+  }
+>(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const [showChatbot, setShowChatbot] = useState<boolean>(false);
+  const theme = useTheme();
   return (
     <>
       <Header />
       <main>
         {children}
+          <div
+            style={{
+              position: 'fixed',
+              bottom: 50,
+              right: 50,
+              cursor: 'pointer'
+            }}
+            onClick={() => setShowChatbot(true)}
+          >
+            <Image
+              src='/logo/chatbot-logo.png'
+              alt='chatbot'
+              width={50}
+              height={50}
+            />
+          </div>
+          <Dialog
+            open={showChatbot}
+            onClose={() => setShowChatbot(false)}
+            TransitionComponent={Transition}
+            keepMounted
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+            BackdropProps={{ invisible: true }}
+            PaperProps={{
+              style: {
+                position: 'fixed',
+                bottom: 30,
+                right: 50,
+                margin: 0,
+                width: '400px',
+                height: '600px',
+                borderRadius: '12px',
+                backgroundColor: theme.palette.text.secondary,
+                
+              },
+            }}
+          >
+            <Typography
+              onClick={() => setShowChatbot(false)}
+              sx={{ position: 'absolute', right: 10, top: 10, cursor: 'pointer' }}
+            >
+              <Cancel sx={{color: theme.palette.text.secondary}} />
+            </Typography>
+            <ChatbotModelView />
+          </Dialog>
       </main>
       <Footer />
     </>

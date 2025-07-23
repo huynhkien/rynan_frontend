4fe70a04961bda438bc0980ab1d498e9cd6f4996
huynhkien <huynhhuukien2962@gmail.com>
2025-07-23 10:49:51 +0700

    Update pages return order vnp to help for payment online

---
+++ src/app/(main)/checkout/return-order-vnp/[status]/page.tsx
@@ -0,0 +1,8 @@
+import { ReturnOrderVNPView } from "@/widgets/public/return-order-vnp/view/return-order-vnp-view"
+
+const ReturnOrderVNP = () => {
+    return (
+        <ReturnOrderVNPView/>
+    )
+}
+export default ReturnOrderVNP;

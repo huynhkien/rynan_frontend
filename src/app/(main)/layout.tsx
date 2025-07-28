7b89ace8700f02fd863c674305f5d3a367a458b9
huynhkien <huynhhuukien2962@gmail.com>
2025-07-28 09:58:01 +0700

    Update layout main home

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -22,54 +22,56 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
       <Header />
       <main>
         {children}
-          <div
-            style={{
-              position: 'fixed',
-              bottom: 50,
-              right: 50,
-              cursor: 'pointer'
-            }}
-            onClick={() => setShowChatbot(true)}
-          >
-            <Image
-              src='/logo/chatbot-logo.png'
-              alt='chatbot'
-              width={50}
-              height={50}
-            />
-          </div>
-          <Dialog
-            open={showChatbot}
-            onClose={() => setShowChatbot(false)}
-            TransitionComponent={Transition}
-            keepMounted
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-            BackdropProps={{ invisible: true }}
-            PaperProps={{
-              style: {
-                position: 'fixed',
-                bottom: 30,
-                right: 50,
-                margin: 0,
-                width: '400px',
-                height: '600px',
-                borderRadius: '12px',
-                backgroundColor: theme.palette.text.secondary,
-                
-              },
-            }}
-          >
-            <Typography
-              onClick={() => setShowChatbot(false)}
-              sx={{ position: 'absolute', right: 10, top: 10, cursor: 'pointer' }}
-            >
-              <Cancel sx={{color: theme.palette.text.secondary}} />
-            </Typography>
-            <ChatbotModelView />
-          </Dialog>
       </main>
       <Footer />
+      <div
+        style={{
+            position: 'fixed',
+            bottom: 50,
+            right: 50,
+            cursor: 'pointer',
+            zIndex: 100
+          }}
+          onClick={() => setShowChatbot(true)}
+        >
+          <Image
+            src='/logo/chatbot-logo.png'
+            alt='chatbot'
+            width={50}
+            height={50}
+          />
+      </div>
+      <Dialog
+        open={showChatbot}
+        onClose={() => setShowChatbot(false)}
+        TransitionComponent={Transition}
+        keepMounted
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        BackdropProps={{ invisible: true }}
+        PaperProps={{
+          style: {
+            position: 'fixed',
+            bottom: 30,
+            right: 50,
+            margin: 0,
+            width: '400px',
+            height: '600px',
+            borderRadius: '12px',
+            backgroundColor: theme.palette.text.secondary,
+            zIndex: 9999
+            
+          },
+        }}
+      >
+        <Typography
+          onClick={() => setShowChatbot(false)}
+          sx={{ position: 'absolute', right: 10, top: 10, cursor: 'pointer' }}
+        >
+          <Cancel sx={{color: theme.palette.text.secondary}} />
+        </Typography>
+        <ChatbotModelView />
+      </Dialog>
     </>
   )
 }

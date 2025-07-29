e77032ee2d0a573c82865b30772880fcca820c6f
huynhkien <huynhhuukien2962@gmail.com>
2025-07-29 14:18:50 +0700

    Update structure providers

--- src/app/_providers/logout-provider.tsx
+++ src/app/_providers/logout-provider.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import useAutoLogout from '@/shared/hooks/useAutoLogout';
+import useAutoRefreshToken from '@/shared/hooks/useAutoRefreshToken';
 import { ReactNode } from 'react';
 
 export const AutoLogoutWrapper = ({ children }: {children: ReactNode}) => {
   useAutoLogout();
-
+  useAutoRefreshToken();
   return <>{children}</>;
 };

ff0b923d849aeba8316ae8538b746706974fb29b
huynhkien <huynhhuukien2962@gmail.com>
2025-07-31 14:46:58 +0700

    Update structure useAutoLogout hook

--- src/shared/hooks/useAutoLogout.ts
+++ src/shared/hooks/useAutoLogout.ts
@@ -7,16 +7,26 @@ const useAutoLogout = () => {
   const timeoutRef = useRef<number | null>(null);
 
   useEffect(() => {
+    // Thời gian đủ để logout
     const TWO_DAYS_IN_MS = 1 * 24 * 60 *60 * 1000;
     const autoLogout = () => {
       dispatch(logout());
       window.location.href = '/';
     };
+    // Logout khi đóng tab
+    const handleBeforeUnload = () => {
+      dispatch(logout());
+    };
+
+    // Thêm event listener
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
     timeoutRef.current = window.setTimeout(autoLogout, TWO_DAYS_IN_MS);
     return () => {
       if (timeoutRef.current) {
         window.clearTimeout(timeoutRef.current);
       }
+      window.removeEventListener('beforeunload', handleBeforeUnload);
     };
   }, [dispatch]);
 };
